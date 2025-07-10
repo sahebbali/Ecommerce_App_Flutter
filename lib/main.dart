8ecd1470e5cbe43164cdb34ed82d6b3943f18c0b
sahebbali <saheb1996jmr@gmail.com>
2025-07-10 16:29:35 +0600

    fixed splash issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'my_app.dart';
 void main() {
+    /// Widgets Binding
+  final WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  /// Await Native Splash
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  
   runApp(const MyApp());
 }
