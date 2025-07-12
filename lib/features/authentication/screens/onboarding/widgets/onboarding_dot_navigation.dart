28ff8c0170123295775dc8d9d4b79e308076a3c0
sahebbali <saheb1996jmr@gmail.com>
2025-07-12 19:46:22 +0600

    fixed some issue

--- lib/features/authentication/screens/onboarding/widgets/onboarding_dot_navigation.dart
+++ lib/features/authentication/screens/onboarding/widgets/onboarding_dot_navigation.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shopping_store/features/authentication/controllers/onboarding/onboarding_controller.dart';
@@ -11,26 +9,25 @@ import '../../../../../utils/constants/colors.dart';
 import '../../../../../utils/device/device_utility.dart';
 
 class OnBoardingDotNavigation extends StatelessWidget {
-  const OnBoardingDotNavigation({
-    super.key,
-  });
+  const OnBoardingDotNavigation({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     final dark = HkHelperFunctions.isDarkMode(context);
     final controller = Get.put(OnBoardingController());
 
     return Positioned(
-        bottom: HkDeviceUtils.getBottomNavigationBarHeight() + 25,
-        left: HkSizes.defaultSpace,
-        child: SmoothPageIndicator(
-          controller: controller.pageController,
-          onDotClicked: controller.dotNavigationClick,
-          count: 3,
-          effect: ExpandingDotsEffect(
-              dotHeight: 6,
-              activeDotColor: dark ? HkColors.light : HkColors.dark),
-        ));
+      bottom: HkDeviceUtils.getBottomNavigationBarHeight() + 25,
+      left: 100,
+      child: SmoothPageIndicator(
+        controller: controller.pageController,
+        onDotClicked: controller.dotNavigationClick,
+        count: 3,
+        effect: ExpandingDotsEffect(
+          dotHeight: 6,
+          activeDotColor: dark ? HkColors.light : HkColors.dark,
+        ),
+      ),
+    );
   }
 }
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_store/features/authentication/screens/onboarding/onboarding.dart';
 import 'package:get/get.dart';
+import 'package:shopping_store/utils/constants/colors.dart';
+import 'package:shopping_store/utils/theme/theme.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -8,8 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: HkAppTheme.lightTheme,
+      darkTheme: HkAppTheme.darkTheme,
+      // initialBinding: GeneralBindings(),
       title: 'Ecommerce App',
-      theme: ThemeData(primarySwatch: Colors.blue),
+
       home: const OnBoardingScreen(),
     );
   }
