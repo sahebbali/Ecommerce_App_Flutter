a8b6f2afd1a4412f77831ef8dc9b56fec8c41c0a
sahebbali <saheb1996jmr@gmail.com>
2025-07-13 17:03:08 +0600

    add navigation manu

---
+++ lib/features/shop/screens/store/store.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class StoreScreen extends StatelessWidget {
+  const StoreScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Store'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Store Screen'),
+      ),
+    );
+  }
+}
---
+++ lib/navigation_menu.dart
@@ -0,0 +1,57 @@
+import 'package:shopping_store/utils/constants/colors.dart';
+import 'package:shopping_store/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+
+import 'features/personalization/screens/profile/profile.dart';
+import 'features/shop/screens/home/home.dart';
+import 'features/shop/screens/store/store.dart';
+import 'features/shop/screens/wishlist/wishlist.dart';
+
+class NavigationMenu extends StatelessWidget {
+  const NavigationMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(NavigationController());
+    bool dark = HkHelperFunctions.isDarkMode(context);
+
+    return Scaffold(
+      body: Obx(() => controller.screens[controller.selectedIndex.value]),
+
+      /// -----[Navigation Menu]-----
+      bottomNavigationBar: Obx(
+        () => NavigationBar(
+          elevation: 0,
+          backgroundColor: dark ? HkColors.dark : HkColors.light,
+          indicatorColor: dark
+              ? HkColors.light.withValues(alpha: 0.1)
+              : HkColors.dark.withValues(alpha: 0.1),
+          selectedIndex: controller.selectedIndex.value,
+          onDestinationSelected: (index) {
+            controller.selectedIndex.value = index;
+          },
+          destinations: [
+            NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Iconsax.shop), label: 'Store'),
+            NavigationDestination(icon: Icon(Iconsax.heart), label: 'Wishlist'),
+            NavigationDestination(icon: Icon(Iconsax.user), label: 'Profile'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NavigationController extends GetxController {
+  static NavigationController get instance => Get.find();
+  RxInt selectedIndex = 0.obs;
+
+  List<Widget> screens = [
+    HomeScreen(),
+    StoreScreen(),
+    WishlistScreen(),
+    ProfileScreen(),
+  ];
+}
