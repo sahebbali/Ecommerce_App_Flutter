e9f8a501c5e02a9c9b688de743e0fe8b0cf5fee1
sahebbali <saheb1996jmr@gmail.com>
2025-07-13 18:03:49 +0600

    add forget password and reset password

---
+++ lib/features/authentication/screens/password_configuration/forget_passwrod.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:shopping_store/features/authentication/controllers/forget_password/forget_password_controller.dart';
+import 'package:shopping_store/features/authentication/screens/password_configuration/reset_password.dart';
+import 'package:shopping_store/utils/constants/text_strings.dart';
+import 'package:shopping_store/utils/validators/validation.dart';
+
+import '../../../../utils/constants/sizes.dart';
+
+class ForgetPassScreen extends StatelessWidget {
+  const ForgetPassScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(ForgetPasswordController());
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(HkSizes.defaultSpace),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+
+              /// Heading
+              Text(HkTexts.forgetPasswordTitle, style: Theme
+                  .of(context)
+                  .textTheme
+                  .headlineMedium,),
+              const SizedBox(height: HkSizes.spaceBtwItems,),
+              Text(HkTexts.forgetPasswordSubTitle, style: Theme
+                  .of(context)
+                  .textTheme
+                  .labelMedium,),
+              const SizedBox(height: HkSizes.spaceBtwSections * 2,),
+
+              /// Text Field
+              Form(
+                key: controller.forgetPasswordFormKey,
+                child: TextFormField(
+                  controller: controller.email,
+                  validator: (value) => HkValidator.validateEmail(value),
+                  decoration: const InputDecoration(
+                      labelText: HkTexts.email, prefixIcon: Icon(Iconsax.direct_right)),),
+              ),
+              const SizedBox(height: HkSizes.spaceBtwSections,),
+
+              /// Submit Button
+              SizedBox(width: double.infinity, child: ElevatedButton(onPressed: () => controller.sendPasswordResetEmail(),
+                child: const Text(HkTexts.submit),),)
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/authentication/screens/password_configuration/reset_password.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:shopping_store/features/authentication/controllers/forget_password/forget_password_controller.dart';
+import 'package:shopping_store/features/authentication/screens/login/login.dart';
+
+import '../../../../utils/constants/image_strings.dart';
+import '../../../../utils/constants/sizes.dart';
+import '../../../../utils/constants/text_strings.dart';
+import '../../../../utils/helpers/helper_functions.dart';
+
+class ResetPasswordScreen extends StatelessWidget {
+  const ResetPasswordScreen({super.key, required this.email});
+
+  final String email;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [IconButton(onPressed: () => Get.back(), icon: const Icon(CupertinoIcons.clear))],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(HkSizes.defaultSpace),
+          child: Column(
+            children: [
+              /// Image
+              Image(
+                image: const AssetImage(HkImages.deliveredEmailIllustration),
+                width: HkHelperFunctions.screenWidth() * 0.6,
+              ),
+              const SizedBox(
+                height: HkSizes.spaceBtwSections,
+              ),
+
+              /// Email, Title & Subtitle
+              Text(
+                email,
+                style: Theme.of(context).textTheme.bodyMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: HkSizes.spaceBtwItems,
+              ),
+              Text(
+                HkTexts.changeYourPasswordTitle,
+                style: Theme.of(context).textTheme.headlineMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: HkSizes.spaceBtwItems,
+              ),
+              Text(
+                HkTexts.changeYourPasswordSubTitle,
+                style: Theme.of(context).textTheme.labelMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: HkSizes.spaceBtwSections,
+              ),
+
+              /// Buttons
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () => Get.offAll(() => const LoginScreen()),
+                  child: const Text(HkTexts.done),
+                ),
+              ),
+              const SizedBox(
+                height: HkSizes.spaceBtwItems,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: TextButton(
+                  onPressed: () => ForgetPasswordController.instance.resendPasswordResetEmail(email),
+                  child: const Text(HkTexts.resendEmail)
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
