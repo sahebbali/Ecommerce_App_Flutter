5d7bdc3b6e81b995989ad309c04c5f3f066ab1c6
sahebbali <saheb1996jmr@gmail.com>
2025-07-14 18:25:57 +0600

    add some

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'my_app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 void main() async {
+//hello
   /// Widgets Binding
   // final WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
 
