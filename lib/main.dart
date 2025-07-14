f963cd90de7d6589cc4de2d2f665d1374903f926
sahebbali <saheb1996jmr@gmail.com>
2025-07-14 18:15:56 +0600

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 // import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'my_app.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+void main() async {
   /// Widgets Binding
   // final WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
 
   /// Await Native Splash
   // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
 
   runApp(const MyApp());
 }
