834bf4df1949da19dbf294622a7039af45caae18
sahebbali <saheb1996jmr@gmail.com>
2025-07-16 21:44:25 +0600

    update my app file

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_store/features/authentication/screens/onboarding/onboarding.dart';
 import 'package:get/get.dart';
+import 'package:shopping_store/bindings/general_bindings.dart';
+import 'package:shopping_store/routes/app_routes.dart';
 import 'package:shopping_store/utils/constants/colors.dart';
 import 'package:shopping_store/utils/theme/theme.dart';
 
+
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.system,
       theme: HkAppTheme.lightTheme,
       darkTheme: HkAppTheme.darkTheme,
-      // initialBinding: GeneralBindings(),
-      title: 'Ecommerce App',
-
-      home: const OnBoardingScreen(),
+      initialBinding: GeneralBindings(),
+      getPages: AppRoutes.pages,
+      /// show loader or circular progress indicator meanwhile Authentication Repository
+        /// is deciding to show relevant screen
+      home: const Scaffold(
+        backgroundColor: HkColors.primary,
+        body: Center(
+          child: CircularProgressIndicator(color: Colors.white,),
+        ),
+      )
     );
   }
 }
