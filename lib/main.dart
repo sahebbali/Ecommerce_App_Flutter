63abfc833d8cb4562a136d380c62a1a87f753e8a
sahebbali <saheb1996jmr@gmail.com>
2025-07-17 15:13:53 +0600

    update main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'my_app.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'data/repositories/authentication/authentication_repository.dart';
 import 'firebase_options.dart';
-void main() async {
+import 'my_app.dart';
+
+void main()  async{
 
   /// Widgets Binding
-  // final WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  final WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+
+  /// -- GetX Local Storage
+  await GetStorage.init();
 
   /// Await Native Splash
-  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-);
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  
+  /// -- Initialize Firebase & Authentication Repository
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,).then(
+          (FirebaseApp value) => Get.put(AuthenticationRepository()));
 
+  // Load all the Material Design / Themes / Localizations / Bindings
   runApp(const MyApp());
+  
 }
