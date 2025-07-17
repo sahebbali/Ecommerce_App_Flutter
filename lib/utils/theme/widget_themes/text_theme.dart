1c66ecec16573944d6d5d69b41009349e0d8fb9c
sahebbali <saheb1996jmr@gmail.com>
2025-07-17 15:39:13 +0600

    update hk thext theme

--- lib/utils/theme/widget_themes/text_theme.dart
+++ lib/utils/theme/widget_themes/text_theme.dart
@@ -7,37 +7,125 @@ class HkTextTheme {
 
   /// Customizable Light Text Theme
   static TextTheme lightTextTheme = TextTheme(
-    headlineLarge: const TextStyle().copyWith(fontSize: 32.0, fontWeight: FontWeight.bold, color: HkColors.dark),
-    headlineMedium: const TextStyle().copyWith(fontSize: 24.0, fontWeight: FontWeight.w600, color: HkColors.dark),
-    headlineSmall: const TextStyle().copyWith(fontSize: 18.0, fontWeight: FontWeight.w600, color: HkColors.dark),
+    headlineLarge: const TextStyle().copyWith(
+      fontSize: 32.0,
+      fontWeight: FontWeight.bold,
+      color: HkColors.dark,
+    ),
+    headlineMedium: const TextStyle().copyWith(
+      fontSize: 24.0,
+      fontWeight: FontWeight.w600,
+      color: HkColors.dark,
+    ),
+    headlineSmall: const TextStyle().copyWith(
+      fontSize: 18.0,
+      fontWeight: FontWeight.w600,
+      color: HkColors.dark,
+    ),
 
-    titleLarge: const TextStyle().copyWith(fontSize: 16.0, fontWeight: FontWeight.w600, color: HkColors.dark),
-    titleMedium: const TextStyle().copyWith(fontSize: 16.0, fontWeight: FontWeight.w500, color: HkColors.dark),
-    titleSmall: const TextStyle().copyWith(fontSize: 16.0, fontWeight: FontWeight.w400, color: HkColors.dark),
+    titleLarge: const TextStyle().copyWith(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w600,
+      color: HkColors.dark,
+    ),
+    titleMedium: const TextStyle().copyWith(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w500,
+      color: HkColors.dark,
+    ),
+    titleSmall: const TextStyle().copyWith(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w400,
+      color: HkColors.dark,
+    ),
 
-    bodyLarge: const TextStyle().copyWith(fontSize: 14.0, fontWeight: FontWeight.w500, color: HkColors.dark),
-    bodyMedium: const TextStyle().copyWith(fontSize: 14.0, fontWeight: FontWeight.normal, color: HkColors.dark),
-    bodySmall: const TextStyle().copyWith(fontSize: 14.0, fontWeight: FontWeight.w500, color: HkColors.dark.withOpacity(0.5)),
+    bodyLarge: const TextStyle().copyWith(
+      fontSize: 14.0,
+      fontWeight: FontWeight.w500,
+      color: HkColors.dark,
+    ),
+    bodyMedium: const TextStyle().copyWith(
+      fontSize: 14.0,
+      fontWeight: FontWeight.normal,
+      color: HkColors.dark,
+    ),
+    bodySmall: const TextStyle().copyWith(
+      fontSize: 14.0,
+      fontWeight: FontWeight.w500,
+      color: HkColors.dark.withAlpha(128),
+    ),
 
-    labelLarge: const TextStyle().copyWith(fontSize: 12.0, fontWeight: FontWeight.normal, color: HkColors.dark),
-    labelMedium: const TextStyle().copyWith(fontSize: 12.0, fontWeight: FontWeight.normal, color: HkColors.dark.withOpacity(0.5)),
+    labelLarge: const TextStyle().copyWith(
+      fontSize: 12.0,
+      fontWeight: FontWeight.normal,
+      color: HkColors.dark,
+    ),
+    labelMedium: const TextStyle().copyWith(
+      fontSize: 12.0,
+      fontWeight: FontWeight.normal,
+      color: HkColors.dark.withAlpha(128),
+    ),
   );
 
   /// Customizable Dark Text Theme
   static TextTheme darkTextTheme = TextTheme(
-    headlineLarge: const TextStyle().copyWith(fontSize: 32.0, fontWeight: FontWeight.bold, color: HkColors.light),
-    headlineMedium: const TextStyle().copyWith(fontSize: 24.0, fontWeight: FontWeight.w600, color: HkColors.light),
-    headlineSmall: const TextStyle().copyWith(fontSize: 18.0, fontWeight: FontWeight.w600, color: HkColors.light),
+    headlineLarge: const TextStyle().copyWith(
+      fontSize: 32.0,
+      fontWeight: FontWeight.bold,
+      color: HkColors.light,
+    ),
+    headlineMedium: const TextStyle().copyWith(
+      fontSize: 24.0,
+      fontWeight: FontWeight.w600,
+      color: HkColors.light,
+    ),
+    headlineSmall: const TextStyle().copyWith(
+      fontSize: 18.0,
+      fontWeight: FontWeight.w600,
+      color: HkColors.light,
+    ),
 
-    titleLarge: const TextStyle().copyWith(fontSize: 16.0, fontWeight: FontWeight.w600, color: HkColors.light),
-    titleMedium: const TextStyle().copyWith(fontSize: 16.0, fontWeight: FontWeight.w500, color: HkColors.light),
-    titleSmall: const TextStyle().copyWith(fontSize: 16.0, fontWeight: FontWeight.w400, color: HkColors.light),
+    titleLarge: const TextStyle().copyWith(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w600,
+      color: HkColors.light,
+    ),
+    titleMedium: const TextStyle().copyWith(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w500,
+      color: HkColors.light,
+    ),
+    titleSmall: const TextStyle().copyWith(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w400,
+      color: HkColors.light,
+    ),
 
-    bodyLarge: const TextStyle().copyWith(fontSize: 14.0, fontWeight: FontWeight.w500, color: HkColors.light),
-    bodyMedium: const TextStyle().copyWith(fontSize: 14.0, fontWeight: FontWeight.normal, color: HkColors.light),
-    bodySmall: const TextStyle().copyWith(fontSize: 14.0, fontWeight: FontWeight.w500, color: HkColors.light.withOpacity(0.5)),
+    bodyLarge: const TextStyle().copyWith(
+      fontSize: 14.0,
+      fontWeight: FontWeight.w500,
+      color: HkColors.light,
+    ),
+    bodyMedium: const TextStyle().copyWith(
+      fontSize: 14.0,
+      fontWeight: FontWeight.normal,
+      color: HkColors.light,
+    ),
+    bodySmall: const TextStyle().copyWith(
+      fontSize: 14.0,
+      fontWeight: FontWeight.w500,
+      color: HkColors.light.withAlpha(128),
+    ),
 
-    labelLarge: const TextStyle().copyWith(fontSize: 12.0, fontWeight: FontWeight.normal, color: HkColors.light),
-    labelMedium: const TextStyle().copyWith(fontSize: 12.0, fontWeight: FontWeight.normal, color: HkColors.light.withOpacity(0.5)),
+    labelLarge: const TextStyle().copyWith(
+      fontSize: 12.0,
+      fontWeight: FontWeight.normal,
+      color: HkColors.light,
+    ),
+    labelMedium: const TextStyle().copyWith(
+      fontSize: 12.0,
+      fontWeight: FontWeight.normal,
+      color: HkColors.light.withAlpha(128),
+    ),
   );
 }
